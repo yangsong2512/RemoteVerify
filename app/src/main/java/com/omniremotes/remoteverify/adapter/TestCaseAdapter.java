b174621e7f9bece4dff7df692865ab8481fb0ee9
Yangsong <yangsong2512@126.com>
2018-11-07 17:55:11 +0800

    {Yangsong} add pairing dialog
    1. add paring dialog

--- app/src/main/java/com/omniremotes/remoteverify/adapter/TestCaseAdapter.java
+++ app/src/main/java/com/omniremotes/remoteverify/adapter/TestCaseAdapter.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.omniremotes.remoteverify.R;
+import com.omniremotes.remoteverify.dialog.PairingDialog;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -166,7 +167,14 @@ public class TestCaseAdapter extends BaseAdapter {
             viewHolder = new ViewHolder();
             viewHolder.titleTextView = convertView.findViewById(R.id.test_case_title);
             viewHolder.descTextView = convertView.findViewById(R.id.test_case_description);
-            //viewHolder.startButton = convertView.findViewById(R.id.test_case_run);
+            viewHolder.startButton = convertView.findViewById(R.id.test_case_run);
+            viewHolder.startButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PairingDialog dialog = new PairingDialog(mContext);
+                    dialog.show();
+                }
+            });
             convertView.setTag(viewHolder);
         }else{
             viewHolder =(ViewHolder) convertView.getTag();
--- app/src/main/java/com/omniremotes/remoteverify/dialog/PairingDialog.java
+++ app/src/main/java/com/omniremotes/remoteverify/dialog/PairingDialog.java
@@ -3,14 +3,40 @@ package com.omniremotes.remoteverify.dialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.omniremotes.remoteverify.R;
 
 public class PairingDialog extends Dialog {
-    PairingDialog(Context context){
+    public PairingDialog(Context context){
         super(context);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initUI();
+    }
+
+    private void initUI(){
+        LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+        View view = layoutInflater.inflate(R.layout.pairing_dialog_layout,null);
+        setContentView(view);
+        TextView titleTextView = view.findViewById(R.id.dialog_title);
+        titleTextView.setText("Pairing Test");
+        Window window = getWindow();
+        if(window==null){
+            return;
+        }
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        DisplayMetrics d = getContext().getResources().getDisplayMetrics();
+        layoutParams.width = (int) (d.widthPixels * 1.0);
+        layoutParams.height = (int) (d.heightPixels*0.5);
+        window.setAttributes(layoutParams);
     }
 }
