fa4292f95a7009384fe87c3c37a90b32bb971f71
yangsong <yangsong2512@126.com>
2018-11-11 20:20:28 +0800

    {Yang.Song}add voice service skeleton
    1. add voice service skeleton

--- app/src/main/java/com/omniremotes/remoteverify/service/VoiceService.java
+++ app/src/main/java/com/omniremotes/remoteverify/service/VoiceService.java
@@ -15,6 +15,36 @@ public class VoiceService extends Service {
         super.onCreate();
     }
 
+    private static class VoiceServiceBinder extends IVoiceService.Stub{
+        VoiceService svc;
+        VoiceServiceBinder(VoiceService service){
+            svc = service;
+        }
+        @Override
+        public void startVoice() {
+            if(svc == null){
+                return;
+            }
+            svc.startVoice();
+        }
+
+        @Override
+        public void stopVoice() {
+            if(svc == null){
+                return;
+            }
+            svc.stopVoice();
+        }
+    }
+
+    private void startVoice(){
+
+    }
+
+    private void stopVoice(){
+
+    }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return super.onStartCommand(intent, flags, startId);
